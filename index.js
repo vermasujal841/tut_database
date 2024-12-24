97f238d2ed73d218f2173893527b2a8779d063f8
Sujal Verma <vermasujal16@gmail.com>
2024-12-24 18:37:44 +0530

    comments

--- index.js
+++ index.js
@@ -24,3 +24,23 @@ app.post("/signup", async function(req,res){
     })
 })
 app.listen(3000);
+
+
+
+// A= CLIENT
+
+// B= SERVER
+
+// The flow:
+
+// 1) A - B: Client sends username and password
+
+// 2) B: Server checks them against DB records and if they match it creates; first, 
+// signature using: base64UrlEncode(header).base64Url(payload), #secret# and then token using: signature.payload.secret
+
+//3) A <- B: Server sends back token to client
+
+//4) A->B: Client sends request to access certain URL with token in header
+
+//5) B: Server decodes header and payload, uses #secret# to create another digital 
+// signature and compares it with what was sent in to ensure integrity
